050486e67f0858194231a8d2748db4da8ce9136c
Widua <mikolaj.widla@gmail.com>
2022-01-03 09:50:31 +0100

    Initial commit, with firt lightly configured polybar, i3wm and terminator configs

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
+
+[bar/mybar]
+override-redirect = true
+wm-restack = i3
